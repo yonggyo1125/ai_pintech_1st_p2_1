087deef0fc7e02ff2373ce9c0c3a7146dac00b67
yonggyo1125 <yonggyo00@kakao.com>
2024-11-13 11:22:06 +0900

    프로젝트 기본 구성

--- src/main/java/org/koreait/main/controllers/MainController.java
+++ src/main/java/org/koreait/main/controllers/MainController.java
@@ -1,16 +1,20 @@
 package org.koreait.main.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/")
 public class MainController {
 
     @GetMapping
-    public String index() {
-
+    public String index(Model model) {
+        model.addAttribute("addCss", List.of("member/test1.css", "member/test2.css"));
+        model.addAttribute("addScript", new String[] {"member/test1.js", "member/test2.js"});
         return "front/main/index";
     }
 }

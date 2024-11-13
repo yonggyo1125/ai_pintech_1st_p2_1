8fe937eb5cd55a7b96e3c3184707add76ffbf6fa
yonggyo1125 <yonggyo00@kakao.com>
2024-11-13 12:21:58 +0900

    프로젝트 기본 구성

---
+++ src/main/java/org/koreait/global/libs/Utils.java
@@ -0,0 +1,11 @@
+package org.koreait.global.libs;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Utils {
+
+    public boolean isMobile() {
+        return false;
+    }
+}

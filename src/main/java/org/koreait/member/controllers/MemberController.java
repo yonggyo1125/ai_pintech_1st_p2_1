52b67ba6f1de5e0f60398bc08f8285efe7448e2f
yonggyo1125 <yonggyo00@kakao.com>
2024-11-15 15:01:35 +0900

    회원가입과 로그인 구성

--- src/main/java/org/koreait/member/controllers/MemberController.java
+++ src/main/java/org/koreait/member/controllers/MemberController.java
@@ -1,8 +1,11 @@
 package org.koreait.member.controllers;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.koreait.global.libs.Utils;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,8 +23,23 @@ public class MemberController {
     }
 
     @PostMapping("/login")
-    public String loginPs() {
+    public String loginPs(@Valid RequestLogin form, Errors errors) {
 
-        return null; // 임시
+        if (errors.hasErrors()) {
+            return utils.tpl("member/login");
+        }
+
+        // 로그인 처리
+
+        /**
+         * 로그인 완료 후 페이지 이동
+         * 1) redirectUrl 값이 전달된 경우는 해당 경로로 이동
+         * 2) 없는 경우는 메인 페이지로 이동
+         *
+         */
+        String redirectUrl = form.getRedirectUrl();
+        redirectUrl = StringUtils.hasText(redirectUrl) ? redirectUrl : "/";
+
+        return "redirect:" + redirectUrl;
     }
 }

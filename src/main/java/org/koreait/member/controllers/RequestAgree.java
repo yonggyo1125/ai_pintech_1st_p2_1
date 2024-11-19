db57bd9481ec6ba9078682ab8c1b219423d3041a
yonggyo1125 <yonggyo00@kakao.com>
2024-11-19 16:46:46 +0900

    회원가입과 로그인 구성

--- src/main/java/org/koreait/member/controllers/RequestAgree.java
+++ src/main/java/org/koreait/member/controllers/RequestAgree.java
@@ -1,14 +1,20 @@
 package org.koreait.member.controllers;
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.AssertTrue;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
 public class RequestAgree {
-    @NotNull
-    private boolean[] requiredTerms; // 필수 약관 동의 여부 / 필수는 반드시 모두 선택 - 갯수 체크만으로도 충분
+    @AssertTrue
+    private boolean requiredTerms1; // 필수 약관 동의 여부
+
+    @AssertTrue
+    private boolean requiredTerms2;
+
+    @AssertTrue
+    private boolean requiredTerms3;
 
     private List<String> optionalTerms; // 선택 약관 동의 여부 - 선택약관은 어떤 약관인지를 구분할 수 있어야 함
 }

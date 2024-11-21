23bd07ebd48b2b500d24d78d71e6cad2755b04f4
yonggyo1125 <yonggyo00@kakao.com>
2024-11-21 16:00:28 +0900

    예외 페이지 처리

---
+++ src/main/java/org/koreait/global/exceptions/CommonException.java
@@ -0,0 +1,20 @@
+package org.koreait.global.exceptions;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+/**
+ * 모든 사용자 정의 예외 상위 클래스
+ *
+ */
+@Getter @Setter
+public class CommonException extends RuntimeException {
+    private HttpStatus status;
+    private boolean errorCode;
+
+    public CommonException(String message, HttpStatus status) {
+        super(message);
+        this.status = status;
+    }
+}

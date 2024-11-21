b973a3421f3bbb19a1b009f118b5c222c9f5ed58
yonggyo1125 <yonggyo00@kakao.com>
2024-11-21 15:01:20 +0900

    회원가입과 로그인 구성

--- src/main/java/org/koreait/member/validators/JoinValidator.java
+++ src/main/java/org/koreait/member/validators/JoinValidator.java
@@ -1,5 +1,6 @@
 package org.koreait.member.validators;
 
+import org.koreait.global.validators.PasswordValidator;
 import org.koreait.member.controllers.RequestAgree;
 import org.koreait.member.controllers.RequestJoin;
 import org.springframework.context.annotation.Lazy;
@@ -8,10 +9,11 @@ import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
 import java.time.LocalDate;
+import java.time.Period;
 
 @Lazy
 @Component
-public class JoinValidator implements Validator {
+public class JoinValidator implements Validator, PasswordValidator {
     @Override
     public boolean supports(Class<?> clazz) {
         return clazz.isAssignableFrom(RequestAgree.class) || clazz.isAssignableFrom(RequestJoin.class);
@@ -19,6 +21,11 @@ public class JoinValidator implements Validator {
 
     @Override
     public void validate(Object target, Errors errors) {
+
+        // 커맨드 객체 검증 실패시에는 추가 검증은 진행 X
+        if (errors.hasErrors()) {
+            return;
+        }
 
         if (target instanceof RequestAgree requestAgree) {
             validateAgree(requestAgree, errors);
@@ -44,11 +51,12 @@ public class JoinValidator implements Validator {
      * @param errors
      */
     private void validateJoin(RequestJoin form, Errors errors) {
+
         /**
          * 1. 이메일 중복 여부 체크
          * 2. 비밀번호 복잡성 - 알파벳 대소문자 각각 1개 이상, 숫자 1개 이상, 특수 문자 포함
          * 3. 비밀번호, 비밀번호 확인 일치 여부
-         * 4. 생년월일을 입력받으면 14세 이상만 가입 가능하게 통제
+         * 4. 생년월일을 입력받으면 만 14세 이상만 가입 가능하게 통제
          */
 
         String email = form.getEmail();
@@ -57,8 +65,24 @@ public class JoinValidator implements Validator {
         LocalDate birthDt = form.getBirthDt();
 
         // 2. 비밀번호 복잡성 S
-
+        if (!alphaCheck(password, false) || !numberCheck(password) || !specialCharsCheck(password)) {
+            errors.rejectValue("password", "Complexity");
+        }
         // 2. 비밀번호 복잡성 E
+
+        // 3. 비밀번호, 비밀번호 확인 일치 여부 S
+        if (!password.equals(confirmPassword)) {
+            errors.rejectValue("confirmPassword", "Mismatch");
+        }
+        // 3. 비밀번호, 비밀번호 확인 일치 여부 E
+
+        // 4. 생년월일을 입력받으면 만 14세 이상만 가입 가능하게 통제 S
+        Period period = Period.between(birthDt, LocalDate.now());
+        int year = period.getYears();
+        if (year < 14) { // 만 14세 미만인 경우
+            errors.rejectValue("birthDt", "UnderAge");
+        }
+        // 4. 생년월일을 입력받으면 만 14세 이상만 가입 가능하게 통제 E
     }
 }
 

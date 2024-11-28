2cfe8195cf085422fe49ecb3bdb7d6386e782070
yonggyo1125 <yonggyo00@kakao.com>
2024-11-28 10:02:47 +0900

    포켓몬 엔티티 구성 및 저장 처리

---
+++ src/main/java/org/koreait/global/entities/BaseEntity.java
@@ -0,0 +1,28 @@
+package org.koreait.global.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter @Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt; // 등록일시
+
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDateTime modifiedAt; // 수정일시
+
+    private LocalDateTime deletedAt; // 삭제일시
+}

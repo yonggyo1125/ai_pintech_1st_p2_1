21ed06da73b2dccc881dde1dd65c8cb9139fca85
yonggyo1125 <yonggyo00@kakao.com>
2024-11-28 10:03:59 +0900

    포켓몬 엔티티 구성 및 저장 처리

--- src/main/java/org/koreait/global/configs/MvcConfig.java
+++ src/main/java/org/koreait/global/configs/MvcConfig.java
@@ -1,10 +1,12 @@
 package org.koreait.global.configs;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableJpaAuditing
 public class MvcConfig implements WebMvcConfigurer {
     /**
      * 정적 경로 설정, CSS, JS, 이미지

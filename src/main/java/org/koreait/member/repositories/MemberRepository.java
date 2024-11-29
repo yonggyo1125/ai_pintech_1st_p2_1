88601451806ba26dcde4c7dd2d47129833a23110
yonggyo1125 <yonggyo00@kakao.com>
2024-11-29 17:23:37 +0900

    회원가입처리

--- src/main/java/org/koreait/member/repositories/MemberRepository.java
+++ src/main/java/org/koreait/member/repositories/MemberRepository.java
@@ -1,6 +1,7 @@
 package org.koreait.member.repositories;
 
 import org.koreait.member.entities.Member;
+import org.koreait.member.entities.QMember;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
@@ -10,4 +11,10 @@ import java.util.Optional;
 public interface MemberRepository extends JpaRepository<Member, Long>, QuerydslPredicateExecutor<Member> {
     @EntityGraph(attributePaths = "authorities")
     Optional<Member> findByEmail(String email);
+
+    default boolean exists(String email) {
+        QMember member = QMember.member;
+
+        return exists(member.email.eq(email));
+    }
 }
--- src/main/java/org/koreait/member/validators/JoinValidator.java
+++ src/main/java/org/koreait/member/validators/JoinValidator.java
@@ -1,8 +1,10 @@
 package org.koreait.member.validators;
 
+import lombok.RequiredArgsConstructor;
 import org.koreait.global.validators.PasswordValidator;
 import org.koreait.member.controllers.RequestAgree;
 import org.koreait.member.controllers.RequestJoin;
+import org.koreait.member.repositories.MemberRepository;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
@@ -13,7 +15,11 @@ import java.time.Period;
 
 @Lazy
 @Component
+@RequiredArgsConstructor
 public class JoinValidator implements Validator, PasswordValidator {
+
+    private final MemberRepository memberRepository;
+
     @Override
     public boolean supports(Class<?> clazz) {
         return clazz.isAssignableFrom(RequestAgree.class) || clazz.isAssignableFrom(RequestJoin.class);
@@ -73,6 +79,11 @@ public class JoinValidator implements Validator, PasswordValidator {
         String password = form.getPassword();
         String confirmPassword = form.getConfirmPassword();
         LocalDate birthDt = form.getBirthDt();
+
+        // 1. 이메일 중복 여부 체크
+        if (memberRepository.exists(email)) {
+            errors.rejectValue("email", "Duplicated");
+        }
 
         // 2. 비밀번호 복잡성 S
         if (!alphaCheck(password, false) || !numberCheck(password) || !specialCharsCheck(password)) {

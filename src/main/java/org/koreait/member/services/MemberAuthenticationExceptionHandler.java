cc2b9359e27e0ea88903cecd3343b29b10c51700
yonggyo1125 <yonggyo00@kakao.com>
2024-12-03 16:07:38 +0900

    로그인 처리

--- src/main/java/org/koreait/member/services/MemberAuthenticationExceptionHandler.java
+++ src/main/java/org/koreait/member/services/MemberAuthenticationExceptionHandler.java
@@ -11,6 +11,16 @@ import java.io.IOException;
 public class MemberAuthenticationExceptionHandler implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-
+        /**
+         * 마이페이지 /mypage -> 로그인 페이지
+         * 관리자 : 응답 코드 401
+         */
+        String uri = request.getRequestURI(); // 요청 주소
+        if (uri.contains("/mypage")) { // 마이페이지
+            response.sendRedirect(request.getContextPath() + "/member/login?redirectUrl=/mypage");
+        } else {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED); // 그외 401
+        }
+        
     }
 }

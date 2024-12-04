3e94e112d0031bf6e286d1f5b2fcc265f30b5454
yonggyo1125 <yonggyo00@kakao.com>
2024-12-04 17:12:53 +0900

    로그인 처리

---
+++ src/main/java/org/koreait/global/advices/CommonRestControllerAdvice.java
@@ -0,0 +1,40 @@
+package org.koreait.global.advices;
+
+import org.koreait.global.exceptions.CommonException;
+import org.koreait.global.rests.JSONData;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
+@RestControllerAdvice(annotations = RestController.class)
+public class CommonRestControllerAdvice {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<JSONData> errorHandler(Exception e) {
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR; // 기본 에러 코드 500
+
+        Object message = e.getMessage();
+
+        if (e instanceof CommonException commonException) {
+            status = commonException.getStatus();
+
+            Map<String, Object> errorMessages = commonException.getErrorMessages();
+            if (errorMessages != null) {
+                message = errorMessages;
+            }
+        }
+
+        JSONData data = new JSONData();
+        data.setSuccess(false);
+        data.setStatus(status);
+        data.setMessage(message);
+
+        e.printStackTrace();
+
+        return ResponseEntity.status(status).body(data);
+    }
+}
--- src/main/java/org/koreait/global/exceptions/CommonException.java
+++ src/main/java/org/koreait/global/exceptions/CommonException.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.http.HttpStatus;
 
+import java.util.Map;
 import java.util.Objects;
 
 /**
@@ -14,9 +15,20 @@ import java.util.Objects;
 public class CommonException extends RuntimeException {
     private HttpStatus status;
     private boolean errorCode;
+    private Map<String, Object> errorMessages;
 
     public CommonException(String message, HttpStatus status) {
         super(message);
         this.status = Objects.requireNonNullElse(status, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    /**
+     * RestController에서 커맨드 객체 검증 실패시 가공한 에러 메세지 정보
+     * @param errorMessages
+     * @param status
+     */
+    public CommonException(Map<String, Object> errorMessages, HttpStatus status) {
+        this.errorMessages = errorMessages;
+        this.status = status;
     }
 }

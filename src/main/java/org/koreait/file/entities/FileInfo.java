aee10fe54dd281c227fd499d87c4c49bd28faa32
yonggyo1125 <yonggyo00@kakao.com>
2024-12-05 13:21:38 +0900

    파일 공통 모듈 개발

---
+++ src/main/java/org/koreait/file/entities/FileInfo.java
@@ -0,0 +1,30 @@
+package org.koreait.file.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Transient;
+import lombok.Data;
+import org.koreait.global.entities.BaseMemberEntity;
+
+@Data
+@Entity
+public class FileInfo extends BaseMemberEntity {
+    @Id @GeneratedValue
+    private Long seq; // 파일 등록 번호
+
+    private String gid; // 파일 그룹
+    private String location;  // 그룹 내에서 위치
+
+    private String fileName; // 업로드시 원 파일명
+    private String extension; // 확장자
+    private String contentType; // 파일 형식  image/png   application/..
+
+    @Transient
+    private String fileUrl; // URL로 파일 접근할 수 있는 주소 - 2차 가공
+
+    @Transient
+    private String filePath; // 파일이 서버에 있는 경로
+
+    private boolean done; // 파일과 연관된 작업이 완료되었는지 여부
+}

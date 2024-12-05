11213e27b122d8cc81aec9e0811c45aba13f6e26
yonggyo1125 <yonggyo00@kakao.com>
2024-12-05 15:42:11 +0900

    파일 공통 모듈 개발

--- src/main/java/org/koreait/global/configs/SwaggerConfig.java
+++ src/main/java/org/koreait/global/configs/SwaggerConfig.java
@@ -1,9 +1,12 @@
 package org.koreait.global.configs;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@OpenAPIDefinition(info=@Info(title="포켓몬 도감 API", description = "/api/file - 파일 API"))
 @Configuration
 public class SwaggerConfig {
     @Bean

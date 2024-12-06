52322bac16f37309a9b390cae7552d041e611baf
yonggyo1125 <yonggyo00@kakao.com>
2024-12-06 17:07:20 +0900

    파일 공통 모듈 개발

--- src/main/resources/static/common/js/fileManager.js
+++ src/main/resources/static/common/js/fileManager.js
@@ -6,7 +6,21 @@ window.addEventListener("DOMContentLoaded", function() {
 
     for (const el of fileUploads) {
         el.addEventListener("click", function() {
+            const {gid, location, single, imageOnly} = this.dataset;
+
+            fileEl.gid = gid;
+            fileEl.location = location;
+            fileEl.imageOnly = imageOnly === 'true';
+            fileEl.single = single === 'true';
+            fileEl.multiple = !fileEl.single;  // false - 단일 파일 선택, true - 여러파일 선택 가능
+
             fileEl.click();
         });
     }
+
+    // 파일 선택시 - change 이벤트 발생
+    fileEl.addEventListener("change", function(e) {
+        // e.currentTarget, e.target
+        console.dir(e.currentTarget.files);
+    });
 });

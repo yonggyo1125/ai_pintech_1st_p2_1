29c98b7a4c18a21577bc1c706e9a2ba88de4151b
yonggyo1125 <yonggyo00@kakao.com>
2024-12-06 12:02:39 +0900

    파일 공통 모듈 개발

--- src/main/java/org/koreait/file/repositories/FileInfoRepository.java
+++ src/main/java/org/koreait/file/repositories/FileInfoRepository.java
@@ -1,46 +1,11 @@
 package org.koreait.file.repositories;
 
-import com.querydsl.core.BooleanBuilder;
-import org.koreait.file.constants.FileStatus;
 import org.koreait.file.entities.FileInfo;
-import org.koreait.file.entities.QFileInfo;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
-import org.springframework.util.StringUtils;
-
-import java.util.List;
-import java.util.Objects;
-
-import static org.springframework.data.domain.Sort.Order.asc;
 
 public interface FileInfoRepository extends JpaRepository<FileInfo, Long>, QuerydslPredicateExecutor<FileInfo> {
 
 
-    default List<FileInfo> getList(String gid, String location, FileStatus status) {
-        status = Objects.requireNonNullElse(status, FileStatus.ALL);
 
-        QFileInfo fileInfo = QFileInfo.fileInfo;
-        BooleanBuilder andBuilder = new BooleanBuilder();
-        andBuilder.and(fileInfo.gid.eq(gid)); // 필수
-
-        if (StringUtils.hasText(location)) { // 선택
-            andBuilder.and(fileInfo.location.eq(location));
-        }
-
-        // 파일 작업 완료 상태
-        if (status != FileStatus.ALL) {
-            andBuilder.and(fileInfo.done.eq(status == FileStatus.DONE));
-        }
-
-        return (List<FileInfo>)findAll(andBuilder, Sort.by(asc("createdAt")));
-    }
-
-    default List<FileInfo> getList(String gid, String location) {
-        return getList(gid, location, FileStatus.DONE);
-    }
-
-    default List<FileInfo> getList(String gid) { // 파일 그룹작업 완료된 파일
-        return getList(gid, null);
-    }
 }
--- src/main/java/org/koreait/file/services/FileInfoService.java
+++ src/main/java/org/koreait/file/services/FileInfoService.java
@@ -1,15 +1,23 @@
 package org.koreait.file.services;
 
+import com.querydsl.core.BooleanBuilder;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.koreait.file.constants.FileStatus;
 import org.koreait.file.entities.FileInfo;
+import org.koreait.file.entities.QFileInfo;
 import org.koreait.file.exceptions.FileNotFoundException;
 import org.koreait.file.repositories.FileInfoRepository;
 import org.koreait.global.configs.FileProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
+import java.util.List;
 import java.util.Objects;
+
+import static org.springframework.data.domain.Sort.Order.asc;
 
 @Service
 @RequiredArgsConstructor
@@ -27,6 +35,33 @@ public class FileInfoService  {
         addInfo(item); // 추가 정보 처리
 
         return item;
+    }
+
+    public List<FileInfo> getList(String gid, String location, FileStatus status) {
+        status = Objects.requireNonNullElse(status, FileStatus.ALL);
+
+        QFileInfo fileInfo = QFileInfo.fileInfo;
+        BooleanBuilder andBuilder = new BooleanBuilder();
+        andBuilder.and(fileInfo.gid.eq(gid)); // 필수
+
+        if (StringUtils.hasText(location)) { // 선택
+            andBuilder.and(fileInfo.location.eq(location));
+        }
+
+        // 파일 작업 완료 상태
+        if (status != FileStatus.ALL) {
+            andBuilder.and(fileInfo.done.eq(status == FileStatus.DONE));
+        }
+
+        return (List<FileInfo>)infoRepository.findAll(andBuilder, Sort.by(asc("createdAt")));
+    }
+
+    public List<FileInfo> getList(String gid, String location) {
+        return getList(gid, location, FileStatus.DONE);
+    }
+
+    public List<FileInfo> getList(String gid) { // 파일 그룹작업 완료된 파일
+        return getList(gid, null);
     }
 
     /**

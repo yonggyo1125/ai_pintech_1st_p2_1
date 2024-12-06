945357781a5094298b87f106d7f54d9356fe9337
yonggyo1125 <yonggyo00@kakao.com>
2024-12-06 17:16:42 +0900

    파일 공통 모듈 개발

--- src/main/resources/static/common/js/fileManager.js
+++ src/main/resources/static/common/js/fileManager.js
@@ -1,3 +1,14 @@
+var commonLib = commonLib ?? {};
+commonLib.fileManager = {
+    /**
+    * 파일 업로드 처리
+    *
+    */
+    upload(files, gid, location, single, imageOnly) {
+
+    }
+
+};
 
 window.addEventListener("DOMContentLoaded", function() {
     const fileUploads = document.getElementsByClassName("file-upload");
@@ -20,7 +31,10 @@ window.addEventListener("DOMContentLoaded", function() {
 
     // 파일 선택시 - change 이벤트 발생
     fileEl.addEventListener("change", function(e) {
-        // e.currentTarget, e.target
-        console.dir(e.currentTarget.files);
+        const files = e.currentTarget.files;
+        const {gid, location, single, imageOnly} = fileEl;
+
+        const { fileManager } = commonLib;
+        fileManager.upload(files, gid, location, single, imageOnly);
     });
 });

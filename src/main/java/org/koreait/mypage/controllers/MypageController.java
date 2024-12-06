e94b0fdff1d6c18cb133a637cb34cce08374e85d
yonggyo1125 <yonggyo00@kakao.com>
2024-12-06 15:55:28 +0900

    파일 공통 모듈 개발

--- src/main/java/org/koreait/mypage/controllers/MypageController.java
+++ src/main/java/org/koreait/mypage/controllers/MypageController.java
@@ -2,15 +2,31 @@ package org.koreait.mypage.controllers;
 
 import lombok.RequiredArgsConstructor;
 import org.koreait.global.libs.Utils;
+import org.koreait.member.entities.Member;
+import org.koreait.member.libs.MemberUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/mypage")
 @RequiredArgsConstructor
 public class MypageController {
     private final Utils utils;
+    private final MemberUtil memberUtil;
+
+    @ModelAttribute("profile")
+    public Member getMember() {
+        return memberUtil.getMember();
+    }
+
+    @ModelAttribute("addCss")
+    public List<String> addCss() {
+        return List.of("mypage/style");
+    }
 
     @GetMapping
     public String index() {

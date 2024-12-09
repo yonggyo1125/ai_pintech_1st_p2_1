dc79ab63fc631e2a2c6b5514eab9e2af3b75e9a9
yonggyo1125 <yonggyo00@kakao.com>
2024-12-09 13:21:39 +0900

    파일 업로드

--- src/main/java/org/koreait/file/services/FileUploadService.java
+++ src/main/java/org/koreait/file/services/FileUploadService.java
@@ -40,6 +40,12 @@ public class FileUploadService {
         List<FileInfo> uploadedItems = new ArrayList<>();
 
         for (MultipartFile file : files) {
+            String contentType = file.getContentType();
+            // 이미지 형식의 파일만 허용하는 경우 - 이미지가 아닌 파일은 건너띄기
+            if (form.isImageOnly() && contentType.indexOf("image/") == -1) {
+                continue;
+            }
+
             // 1. 파일 업로드 정보 - DB에 기록 S
             // 파일명.확장자 // model.weights.h5
             String fileName = file.getOriginalFilename();
@@ -50,7 +56,7 @@ public class FileUploadService {
             item.setLocation(location);
             item.setFileName(fileName);
             item.setExtension(extension);
-            item.setContentType(file.getContentType());
+            item.setContentType(contentType);
 
             fileInfoRepository.saveAndFlush(item);
 

e2567adf17a2edff8a3c82f203b6963ff1b43dab
yonggyo1125 <yonggyo00@kakao.com>
2024-12-09 09:53:36 +0900

    추천 시스템

--- src/main/java/org/koreait/dl/entities/TrainItem.java
+++ src/main/java/org/koreait/dl/entities/TrainItem.java
@@ -1,6 +1,8 @@
 package org.koreait.dl.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,6 +15,8 @@ import org.koreait.global.entities.BaseEntity;
 @NoArgsConstructor
 @AllArgsConstructor // 빌더 패턴일때 기본 생성자가 접근 가능해야 하는 경우
 public class TrainItem extends BaseEntity {
+    @Id @GeneratedValue
+    private Long seq;
     private int item1;
     private int item2;
     private int item3;

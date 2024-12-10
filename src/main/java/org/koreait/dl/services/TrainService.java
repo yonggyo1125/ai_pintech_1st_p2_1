f2137f2a3e73614670403d675b233791525b3904
yonggyo1125 <yonggyo00@kakao.com>
2024-12-10 10:45:38 +0900

    .

--- src/main/java/org/koreait/dl/services/TrainService.java
+++ src/main/java/org/koreait/dl/services/TrainService.java
@@ -1,13 +1,23 @@
 package org.koreait.dl.services;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TrainService {
 
+    @Value("${python.run.path}")
+    private String runPath;
+
+    @Value("${python.script.path}")
+    private String scriptPath;
+
+    @Value("${python.data.url}")
+    private String dataUrl;
+
     public void process() {
         try {
-            ProcessBuilder builder = new ProcessBuilder("C:\\Users\\admin\\AppData\\Local\\Programs\\Python\\Python39\\python.exe", "D:/recommend/train.py", "http://localhost:3000/api/dl/data");
+            ProcessBuilder builder = new ProcessBuilder(runPath, scriptPath + "train.py", dataUrl);
             Process process = builder.start();
             int exitCode = process.waitFor();
 

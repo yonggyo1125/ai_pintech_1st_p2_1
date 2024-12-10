ef00471b62c44d201ce1d1df3fe963ff7f43b534
yonggyo1125 <yonggyo00@kakao.com>
2024-12-10 11:21:40 +0900

    .

--- src/main/java/org/koreait/dl/controllers/ApiDlController.java
+++ src/main/java/org/koreait/dl/controllers/ApiDlController.java
@@ -1,17 +1,23 @@
 package org.koreait.dl.controllers;
 
+import lombok.RequiredArgsConstructor;
 import org.koreait.dl.entities.TrainItem;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.koreait.dl.services.PredictService;
+import org.koreait.global.rests.JSONData;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Random;
 import java.util.stream.IntStream;
 
+@Profile("dl")
 @RestController
 @RequestMapping("/api/dl")
+@RequiredArgsConstructor
 public class ApiDlController {
+
+    private final PredictService predictService;
 
     @GetMapping("/data")
     public List<TrainItem> sendData() {
@@ -32,5 +38,13 @@ public class ApiDlController {
                         .build()
                 ).toList();
         return items;
+    }
+
+    @PostMapping("/predict")
+    public JSONData predict(@RequestParam("items") List<int[]> items) {
+
+        int[] predictions = predictService.predict(items);
+
+        return new JSONData(items);
     }
 }
--- src/test/java/org/koreait/dl/tests/PythonRunTest.java
+++ src/test/java/org/koreait/dl/tests/PythonRunTest.java
@@ -1,14 +1,21 @@
 package org.koreait.dl.tests;
 
 import org.junit.jupiter.api.Test;
+import org.koreait.dl.services.PredictService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.io.BufferedReader;
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootTest
-@ActiveProfiles({"default", "test"})
+@ActiveProfiles({"default", "test", "dl"})
 public class PythonRunTest {
+
+    @Autowired
+    private PredictService service;
 
     @Test
     void test1() throws Exception {
@@ -30,5 +37,16 @@ public class PythonRunTest {
         reader.lines().forEach(System.out::println);
         int exitCode = process.waitFor();
         System.out.println(exitCode);
+    }
+
+    @Test
+    void test3() {
+       // python predict.py "[[ 1.23151481,  0.88790998,  1.6140196,   0.94127238,  1.6784415,   1.38504672, -1.57161094, -0.65513703,  0.99961796 , -0.80484811]]"
+
+       int[] item =  { 123151481,  88790998,  16140196,   94127238,  16784415,   138504672, -157161094, -65513703,  99961796 , -80484811};
+        List<int[]> items = List.of(item);
+
+        int[] predictions = service.predict(items);
+        System.out.println(Arrays.toString(predictions));
     }
 }

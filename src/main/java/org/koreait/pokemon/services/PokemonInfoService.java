2d2f4a72f65bcdf857cb26b4f09f8427e8f887f3
yonggyo1125 <yonggyo00@kakao.com>
2024-12-10 17:21:34 +0900

    .

--- src/main/java/org/koreait/pokemon/services/PokemonInfoService.java
+++ src/main/java/org/koreait/pokemon/services/PokemonInfoService.java
@@ -1,13 +1,22 @@
 package org.koreait.pokemon.services;
 
+import com.querydsl.core.BooleanBuilder;
 import lombok.RequiredArgsConstructor;
 import org.koreait.global.paging.ListData;
 import org.koreait.pokemon.controllers.PokemonSearch;
 import org.koreait.pokemon.entities.Pokemon;
+import org.koreait.pokemon.entities.QPokemon;
 import org.koreait.pokemon.exceptions.PokemonNotFoundException;
 import org.koreait.pokemon.repositories.PokemonRepository;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import static org.springframework.data.domain.Sort.Order.desc;
 
 @Lazy
 @Service
@@ -23,6 +32,25 @@ public class PokemonInfoService {
      * @return
      */
     public ListData<Pokemon> getList(PokemonSearch search) {
+        int page = Math.max(search.getPage(), 1); // 페이지 번호
+        int limit = search.getLimit(); // 한페이지 당 레코드 갯수
+        limit = limit < 1 ? 20 : limit;
+
+        QPokemon pokemon = QPokemon.pokemon;
+
+        /* 검색 처리 S */
+        BooleanBuilder andBuilder = new BooleanBuilder();
+        String skey = search.getSkey();
+        if (StringUtils.hasText(skey)) { // 키워드 검색
+            andBuilder.and(pokemon.name
+                    .concat(pokemon.nameEn)
+                    .concat(pokemon.flavorText).contains(skey));
+        }
+        /* 검색 처리 E */
+
+        Pageable pageable = PageRequest.of(page - 1, limit, Sort.by(desc("seq")));
+
+        Page<Pokemon> data = pokemonRepository.findAll(andBuilder, pageable);
 
         return null;
     }

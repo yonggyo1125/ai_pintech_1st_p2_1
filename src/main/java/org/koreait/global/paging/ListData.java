b936f3d8625bde051d090d3b053cee4928f3be89
yonggyo1125 <yonggyo00@kakao.com>
2024-12-10 17:05:15 +0900

    .

--- src/main/java/org/koreait/global/paging/ListData.java
+++ src/main/java/org/koreait/global/paging/ListData.java
@@ -1,8 +1,12 @@
 package org.koreait.global.paging;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ListData<T> {
     private T items; // 목록 데이터
     private Pagination pagination; // 페이징 기초 데이터
--- src/main/java/org/koreait/pokemon/services/PokemonInfoService.java
+++ src/main/java/org/koreait/pokemon/services/PokemonInfoService.java
@@ -1,4 +1,48 @@
 package org.koreait.pokemon.services;
 
+import lombok.RequiredArgsConstructor;
+import org.koreait.global.paging.ListData;
+import org.koreait.pokemon.controllers.PokemonSearch;
+import org.koreait.pokemon.entities.Pokemon;
+import org.koreait.pokemon.repositories.PokemonRepository;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Service;
+
+@Lazy
+@Service
+@RequiredArgsConstructor
 public class PokemonInfoService {
+
+    private final PokemonRepository pokemonRepository;
+
+    /**
+     * 포켓몬 목록 조회
+     *
+     * @param search
+     * @return
+     */
+    public ListData<Pokemon> getList(PokemonSearch search) {
+
+        return null;
+    }
+
+    /**
+     * 포켓몬 단일 조회
+     *
+     * @param seq
+     * @return
+     */
+    public Pokemon get(Long seq) {
+
+        return null;
+    }
+
+    /**
+     * 추가 정보 처리
+     *
+     * @param item
+     */
+    private void addInfo(Pokemon item) {
+
+    }
 }

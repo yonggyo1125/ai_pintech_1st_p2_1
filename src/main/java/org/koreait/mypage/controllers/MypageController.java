0941137b61f047393c820bc9e5394b13acec9135
yonggyo1125 <yonggyo00@kakao.com>
2024-12-10 12:09:50 +0900

    .

--- src/main/java/org/koreait/mypage/controllers/MypageController.java
+++ src/main/java/org/koreait/mypage/controllers/MypageController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.koreait.global.libs.Utils;
 import org.koreait.member.entities.Member;
 import org.koreait.member.libs.MemberUtil;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Controller
@@ -23,6 +25,7 @@ import java.util.List;
 public class MypageController {
     private final Utils utils;
     private final MemberUtil memberUtil;
+    private final ModelMapper modelMapper;
 
     @ModelAttribute("profile")
     public Member getMember() {
@@ -42,12 +45,17 @@ public class MypageController {
     }
 
     @GetMapping("/profile")
-    public String profile(@ModelAttribute RequestProfile form, Model model) {
+    public String profile(Model model) {
         commonProcess("profile", model);
 
         Member member = memberUtil.getMember();
-        form.setName(member.getName());
-        form.setNickName(member.getNickName());
+        RequestProfile form = modelMapper.map(member, RequestProfile.class);
+        String optionalTerms = member.getOptionalTerms();
+        if (StringUtils.hasText(optionalTerms)) {
+            form.setOptionalTerms(Arrays.stream(optionalTerms.split("||")).toList());
+        }
+
+        model.addAttribute("requestProfile", form);
 
         return utils.tpl("mypage/profile");
     }

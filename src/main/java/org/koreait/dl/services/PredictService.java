52bcc633bb7077ce8db9ae4518a30d5c8f3a5e7f
yonggyo1125 <yonggyo00@kakao.com>
2024-12-10 11:08:38 +0900

    .

--- src/main/java/org/koreait/dl/services/PredictService.java
+++ src/main/java/org/koreait/dl/services/PredictService.java
@@ -1,9 +1,14 @@
 package org.koreait.dl.services;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
+
+import java.io.InputStream;
+import java.util.List;
 
 @Lazy
 @Service
@@ -14,4 +19,23 @@ public class PredictService {
 
     @Value("${python.script.path}")
     private String scriptPath;
+
+    @Autowired
+    private ObjectMapper om;
+
+    public int[] predict(List<int[]> items) {
+        try {
+            String data = om.writeValueAsString(items);
+
+            ProcessBuilder builder = new ProcessBuilder(runPath, scriptPath + "predict.py", data);
+            Process process = builder.start();
+            InputStream in = process.getInputStream();
+
+            return om.readValue(in.readAllBytes(), int[].class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
 }

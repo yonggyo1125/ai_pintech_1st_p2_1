efbd3c2afe73fc49a31f4f6090aba137eab6dd73
yonggyo1125 <yonggyo00@kakao.com>
2024-12-11 10:19:24 +0900

    .

--- src/main/java/org/koreait/dl/services/TrainService.java
+++ src/main/java/org/koreait/dl/services/TrainService.java
@@ -1,18 +1,30 @@
 package org.koreait.dl.services;
 
 import lombok.RequiredArgsConstructor;
+import org.koreait.dl.entities.QTrainItem;
 import org.koreait.dl.entities.TrainItem;
+import org.koreait.dl.repositories.TrainItemRepository;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.domain.Sort;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+
+import static org.springframework.data.domain.Sort.Order.asc;
 
 @Lazy
 @Service
 @Profile("dl")
 @RequiredArgsConstructor
 public class TrainService {
+
+    private final TrainItemRepository repository;
 
     @Value("${python.run.path}")
     private String runPath;
@@ -34,6 +46,18 @@ public class TrainService {
     }
 
     public void log(TrainItem item) {
+        repository.saveAndFlush(item);
+    }
 
+
+    public List<TrainItem> getList(boolean isAll) {
+
+
+        if (isAll) {
+            return repository.findAll(Sort.by(asc("createdAt")));
+        } else {
+            QTrainItem trainItem = QTrainItem.trainItem;
+            return (List<TrainItem>)repository.findAll(trainItem.createdAt.after(LocalDateTime.of(LocalDate.now().minusDays(1L), LocalTime.of(0, 0, 0))), Sort.by(asc("createdAt")));
+        }
     }
 }

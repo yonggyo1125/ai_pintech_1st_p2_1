fe7d2df26ef83671cef6ef90fb6e7ea73281d030
yonggyo1125 <yonggyo00@kakao.com>
2024-12-11 10:23:39 +0900

    .

--- src/main/java/org/koreait/dl/controllers/ApiDlController.java
+++ src/main/java/org/koreait/dl/controllers/ApiDlController.java
@@ -3,13 +3,12 @@ package org.koreait.dl.controllers;
 import lombok.RequiredArgsConstructor;
 import org.koreait.dl.entities.TrainItem;
 import org.koreait.dl.services.PredictService;
+import org.koreait.dl.services.TrainService;
 import org.koreait.global.rests.JSONData;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Random;
-import java.util.stream.IntStream;
 
 @Profile("dl")
 @RestController
@@ -18,25 +17,12 @@ import java.util.stream.IntStream;
 public class ApiDlController {
 
     private final PredictService predictService;
+    private final TrainService trainService;
 
     @GetMapping("/data")
-    public List<TrainItem> sendData() {
-        Random random = new Random();
-        List<TrainItem> items = IntStream.range(0, 1000)
-                .mapToObj(i -> TrainItem.builder()
-                        .item1(random.nextInt())
-                        .item2(random.nextInt())
-                        .item3(random.nextInt())
-                        .item4(random.nextInt())
-                        .item5(random.nextInt())
-                        .item6(random.nextInt())
-                        .item7(random.nextInt())
-                        .item8(random.nextInt())
-                        .item9(random.nextInt())
-                        .item10(random.nextInt())
-                        .result(random.nextInt(4))
-                        .build()
-                ).toList();
+    public List<TrainItem> sendData(@RequestParam(name="mode", required = false) String mode) {
+        List<TrainItem> items = trainService.getList(mode != null && mode.equals("ALL"));
+
         return items;
     }
 
@@ -45,6 +31,6 @@ public class ApiDlController {
 
         int[] predictions = predictService.predict(items);
 
-        return new JSONData(items);
+        return new JSONData(predictions);
     }
 }

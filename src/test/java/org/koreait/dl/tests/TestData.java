083cd55538fa91c86f224b8f882397a83d32c66f
yonggyo1125 <yonggyo00@kakao.com>
2024-12-12 09:39:20 +0900

    .

--- src/test/java/org/koreait/dl/tests/TestData.java
+++ src/test/java/org/koreait/dl/tests/TestData.java
@@ -3,6 +3,7 @@ package org.koreait.dl.tests;
 import org.junit.jupiter.api.Test;
 import org.koreait.dl.entities.TrainItem;
 import org.koreait.dl.repositories.TrainItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
@@ -11,6 +12,7 @@ import java.util.stream.IntStream;
 
 @SpringBootTest
 public class TestData {
+    @Autowired
     private TrainItemRepository repository;
 
     @Test

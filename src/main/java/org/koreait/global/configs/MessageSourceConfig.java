bd57cfe89c1470b7b0a1da211418344b36932224
yonggyo1125 <yonggyo00@kakao.com>
2024-12-12 13:20:47 +0900

    .

--- src/main/java/org/koreait/global/configs/MessageSourceConfig.java
+++ src/main/java/org/koreait/global/configs/MessageSourceConfig.java
@@ -11,7 +11,7 @@ public class MessageSourceConfig {
     @Bean
     public MessageSource messageSource() {
         ResourceBundleMessageSource ms = new ResourceBundleMessageSource();
-        ms.addBasenames("messages.commons", "messages.validations", "messages.errors");
+        ms.addBasenames("messages.commons", "messages.validations", "messages.errors", "messages.pokemon");
         ms.setDefaultEncoding("UTF-8");
         ms.setUseCodeAsDefaultMessage(true);
 
--- src/main/java/org/koreait/pokemon/services/PokemonInfoService.java
+++ src/main/java/org/koreait/pokemon/services/PokemonInfoService.java
@@ -22,7 +22,7 @@ import org.springframework.util.StringUtils;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.springframework.data.domain.Sort.Order.desc;
+import static org.springframework.data.domain.Sort.Order.asc;
 
 @Lazy
 @Service
@@ -42,7 +42,7 @@ public class PokemonInfoService {
     public ListData<Pokemon> getList(PokemonSearch search) {
         int page = Math.max(search.getPage(), 1); // 페이지 번호
         int limit = search.getLimit(); // 한페이지 당 레코드 갯수
-        limit = limit < 1 ? 20 : limit;
+        limit = limit < 1 ? 18 : limit;
 
         QPokemon pokemon = QPokemon.pokemon;
 
@@ -56,7 +56,7 @@ public class PokemonInfoService {
         }
         /* 검색 처리 E */
 
-        Pageable pageable = PageRequest.of(page - 1, limit, Sort.by(desc("seq")));
+        Pageable pageable = PageRequest.of(page - 1, limit, Sort.by(asc("seq")));
 
         Page<Pokemon> data = pokemonRepository.findAll(andBuilder, pageable);
         List<Pokemon> items = data.getContent(); // 조회된 목록

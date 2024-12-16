cbc5c8181dc3f56a89e53e085982aed3444d3bf2
yonggyo1125 <yonggyo00@kakao.com>
2024-12-16 17:20:34 +0900

    .

--- src/test/java/org/koreait/email/EmailSendTest.java
+++ src/test/java/org/koreait/email/EmailSendTest.java
@@ -2,6 +2,8 @@ package org.koreait.email;
 
 import jakarta.mail.internet.MimeMessage;
 import org.junit.jupiter.api.Test;
+import org.koreait.email.controllers.RequestEmail;
+import org.koreait.email.services.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -9,6 +11,10 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.test.context.ActiveProfiles;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.spring6.SpringTemplateEngine;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @SpringBootTest
 @ActiveProfiles({"default", "test", "email"})
@@ -18,6 +24,9 @@ public class EmailSendTest {
 
     @Autowired
     private SpringTemplateEngine templateEngine;
+
+    @Autowired
+    private EmailService service;
 
     @Test
     void test1() throws Exception {
@@ -42,5 +51,22 @@ public class EmailSendTest {
         String text = templateEngine.process("email/auth", context);
 
         System.out.println(text);
+    }
+
+    @Test
+    void test3() {
+        RequestEmail form = new RequestEmail();
+        form.setTo(List.of("yonggyo00@kakao.com", "yonggyo00@kakao.com"));
+        form.setCc(List.of("yonggyo1981@gmail.com"));
+        form.setBcc(List.of("yonggyo1981@gmail.com"));
+        form.setSubject("테스트 이메일 제목...");
+        form.setContent("<h1>테스트 이메일 내용...</h1>");
+
+        Map<String, Object> tplData = new HashMap<>();
+        tplData.put("key1", "값1");
+        tplData.put("key2", "값2");
+
+        boolean result = service.sendEmail(form, "auth", tplData);
+        System.out.println(result);
     }
 }

c01b814f0e5223ae6d01c04825b3bc04d311a367
yonggyo1125 <yonggyo00@kakao.com>
2024-12-16 15:36:55 +0900

    .

--- src/main/java/org/koreait/global/configs/I18NConfig.java
+++ src/main/java/org/koreait/global/configs/I18NConfig.java
@@ -1,8 +1,25 @@
 package org.koreait.global.configs;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 @Configuration
-public class I18NConfig {
+public class I18NConfig implements WebMvcConfigurer {
 
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+                //.addPathPatterns("/**") - 사이트 전역에 적용하는 패턴일 경우 생략 가능
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
+        interceptor.setParamName("language"); // ?language=en
+
+        return interceptor;
+    }
 }

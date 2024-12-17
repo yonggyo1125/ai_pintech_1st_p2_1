91885ffb7b12857a59b5119727bfee8d6a99231f
yonggyo1125 <yonggyo00@kakao.com>
2024-12-17 18:13:10 +0900

    .

--- src/main/resources/static/front/js/member/join.js
+++ src/main/resources/static/front/js/member/join.js
@@ -1,7 +1,8 @@
 window.addEventListener("DOMContentLoaded", function() {
 
     const sendButton = document.getElementById("send-auth-code");
-    const resendButton = document.getElementById("resend-auth-code");
+    const authCodeEl = document.getElementById("auth-code");
+
     const { emailAuth } = commonLib;
 
     frmJoin.email.addEventListener("change", function() {
@@ -22,16 +23,19 @@ window.addEventListener("DOMContentLoaded", function() {
         /**
          * 코드 전송 완료 후 후속 처리
          *
-         * 1. 전송 버튼은 감추고, 재전송 버튼은 노출
+         * 1. 전송버튼의 문구를 인증코드 재전송으로 변경
          * 2. 타이머에 남은 시간을 출력
          */
 
         emailAuth.sendCode(email, updateTimer, function() {
-             // 1. 전송 버튼은 감추고, 재전송 버튼은 노출
-             sendButton.classList.remove("dn");
-             sendButton.classList.add("dn");
+             // 1. 전송버튼의 문구를 인증코드 재전송으로 변경,
+             //    이메일을 변경하지 못하게 처리
+             //    인증코드 입력 가능하게 처리
+             const { text } = sendButton.dataset;
+             sendButton.innerText = text;
 
-             resendButton.classList.remove("dn");
+             frmJoin.email.readonly = true;
+             authCodeEl.disabled = false;
         });
     });
 
@@ -45,6 +49,8 @@ window.addEventListener("DOMContentLoaded", function() {
             const min = Math.floor(seconds / 60);
             const sec = seconds - min * 60;
             timeStr = `${('' + min).padStart(2, '0')}:${('' + sec).padStart(2, '0')}`;
+        } else { // 타이머가 0이 되면 다시 이메일 변경 가능하게 처리
+
         }
 
         const timerEl = document.querySelector(".auth-box .timer");

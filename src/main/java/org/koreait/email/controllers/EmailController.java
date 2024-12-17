3edcf0acd0b9e6130aff86b1ac41722372db6cc8
yonggyo1125 <yonggyo00@kakao.com>
2024-12-17 15:48:27 +0900

    .

--- src/main/java/org/koreait/email/controllers/EmailController.java
+++ src/main/java/org/koreait/email/controllers/EmailController.java
@@ -1,6 +1,7 @@
 package org.koreait.email.controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.koreait.email.exceptions.AuthCodeIssueException;
 import org.koreait.email.services.EmailAuthService;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpStatus;
@@ -13,11 +14,26 @@ import org.springframework.web.bind.annotation.*;
 public class EmailController {
     private final EmailAuthService authService;
 
+    /**
+     * 인증코드 발급
+     *
+     * @param to
+     */
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @GetMapping("/auth/{to}")
     public void authCode(@PathVariable("to") String to) {
         if (!authService.sendCode(to)) {
-
+            throw new AuthCodeIssueException();
         }
+    }
+
+    /**
+     * 발급받은 인증코드 검증
+     *
+     * @param authCode
+     */
+    @GetMapping("/verify")
+    public void verify(@RequestParam(name="authCode", required = false) Integer authCode) {
+        authService.verify(authCode);
     }
 }

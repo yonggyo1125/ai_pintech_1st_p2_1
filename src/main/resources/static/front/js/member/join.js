02edacf1f1203a0d32cf354fb42f577a416fac93
yonggyo1125 <yonggyo00@kakao.com>
2024-12-17 17:22:33 +0900

    .

--- src/main/resources/static/front/js/member/join.js
+++ src/main/resources/static/front/js/member/join.js
@@ -1,3 +1,23 @@
+window.addEventListener("DOMContentLoaded", function() {
+
+    const sendButton = document.getElementById("send-auth-code");
+    const resendButton = document.getElementById("resend-auth-code");
+    const { emailAuth } = commonLib;
+
+    frmJoin.email.addEventListener("change", function() {
+        if (this.value.trim()) {
+            sendButton.disabled = false;
+        } else {
+            sendButton.disabled = true;
+        }
+    });
+
+    // 인증코드 전송 버튼 처리
+    sendButton.addEventListener("click", function() {
+
+    });
+});
+
 /**
 * 주소 검색 후 후속 처리 (회원 가입 양식)
 *

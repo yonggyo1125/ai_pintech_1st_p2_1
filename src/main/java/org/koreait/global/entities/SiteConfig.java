25f4fa63e85c4e1e56bd5a4318a69a740c526cb4
yonggyo1125 <yonggyo00@kakao.com>
2024-12-24 15:01:41 +0900

    .

---
+++ src/main/java/org/koreait/global/entities/SiteConfig.java
@@ -0,0 +1,15 @@
+package org.koreait.global.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class SiteConfig {
+    private String siteTitle;
+    private String description;
+    private String keyword;
+    private int cssVersion;
+    private int jsVersion;
+    private boolean useEmailAuth;
+}

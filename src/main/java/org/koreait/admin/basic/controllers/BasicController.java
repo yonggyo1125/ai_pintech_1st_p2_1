f787ed602ee2e70950ab70f93b151e30c6198057
yonggyo1125 <yonggyo00@kakao.com>
2024-12-24 15:13:14 +0900

    .

--- src/main/java/org/koreait/admin/basic/controllers/BasicController.java
+++ src/main/java/org/koreait/admin/basic/controllers/BasicController.java
@@ -4,10 +4,13 @@ import lombok.RequiredArgsConstructor;
 import org.koreait.admin.global.menu.MenuDetail;
 import org.koreait.admin.global.menu.Menus;
 import org.koreait.global.annotations.ApplyErrorPage;
+import org.koreait.global.entities.SiteConfig;
+import org.koreait.global.services.CodeValueService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -17,6 +20,8 @@ import java.util.List;
 @RequiredArgsConstructor
 @RequestMapping("/admin/basic")
 public class BasicController {
+
+                private final CodeValueService codeValueService;
 
     @ModelAttribute("menuCode")
     public String menuCode() {
@@ -32,7 +37,19 @@ public class BasicController {
     public String siteConfig(Model model) {
         commonProcess("siteConfig", model);
 
+        SiteConfig form = codeValueService.get("siteConfig", SiteConfig.class);
+        model.addAttribute("siteConfig", form);
+
         return "admin/basic/siteConfig";
+    }
+
+    @PatchMapping("/siteConfig")
+    public String siteConfigPs(SiteConfig form, Model model) {
+        commonProcess("siteConfig", model);
+
+        codeValueService.save("siteConfig", form);
+
+        return "admin/basic/siteConfig"; // 임시
     }
 
     /**
---
+++ src/test/java/org/koreait/global/services/CodeValueServiceTest.java
@@ -0,0 +1,29 @@
+package org.koreait.global.services;
+
+import org.junit.jupiter.api.Test;
+import org.koreait.global.entities.SiteConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles({"default", "test"})
+public class CodeValueServiceTest {
+
+    @Autowired
+    private CodeValueService service;
+
+    @Test
+    void saveTest() {
+        SiteConfig item = new SiteConfig();
+        item.setSiteTitle("제목");
+        item.setDescription("설명");
+        item.setKeyword("키워드1,키워드2");
+
+        service.save("siteConfig", item);
+
+
+        SiteConfig item2 = service.get("siteConfig", SiteConfig.class);
+        System.out.println(item2);
+    }
+}

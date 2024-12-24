902c50570282d2da083785a92ea3991b2862fff5
yonggyo1125 <yonggyo00@kakao.com>
2024-12-24 15:18:31 +0900

    .

--- src/main/java/org/koreait/global/entities/SiteConfig.java
+++ src/main/java/org/koreait/global/entities/SiteConfig.java
@@ -8,7 +8,7 @@ import lombok.Data;
 public class SiteConfig {
     private String siteTitle;
     private String description;
-    private String keyword;
+    private String keywords;
     private int cssVersion;
     private int jsVersion;
     private boolean useEmailAuth;

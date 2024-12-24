a4fdb1e8cd06ad82403e61f650634b1a5dac40a6
yonggyo1125 <yonggyo00@kakao.com>
2024-12-24 17:17:34 +0900

    .

--- src/main/java/org/koreait/global/entities/Terms.java
+++ src/main/java/org/koreait/global/entities/Terms.java
@@ -1,9 +1,17 @@
 package org.koreait.global.entities;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class Terms {
+
+    @NotBlank
+    private String code; // 약관 코드 terms_{code}
+
+    @NotBlank
     private String subject; // 약관 제목
+
+    @NotBlank
     private String content; // 약관 내용
 }

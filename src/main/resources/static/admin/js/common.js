a5045b290445e504cb2f4020be0e0c8301af1f61
yonggyo1125 <yonggyo00@kakao.com>
2024-12-26 16:42:52 +0900

    .

---
+++ src/main/resources/static/admin/js/common.js
@@ -0,0 +1,27 @@
+window.addEventListener("DOMContentLoaded", function() {
+    // 공통 양식 버튼 처리 S
+    const tableActions = document.getElementsByClassName("table-action");
+    for (const tableAction of tableActions) {
+        const { formName } = tableAction.dataset;
+        const frm = document[frmName];
+        if (!frm) continue;
+
+        const buttons = frm.querySelectorAll("button");
+
+        for (const button of buttons) {
+            button.addEventListener("click", function() {
+                let method = "PATCH";
+                if (this.classList.contains("delete")) {
+                    method = "DELETE";
+                }
+
+                if (confirm(`정말 ${method === 'DELETE' ? '삭제' : '수정'} 하겠습니까?`)) {
+                    frm._method.value = method;
+                    frm.submit();
+                }
+            });
+        }
+
+    }
+    // 공통 양식 버튼 처리 E
+});

95123c884fd14ca8bc03b23c65fb4f812a99945b
yonggyo1125 <yonggyo00@kakao.com>
2024-12-27 11:44:48 +0900

    .

--- src/main/java/org/koreait/admin/member/controllers/MemberSearch.java
+++ src/main/java/org/koreait/admin/member/controllers/MemberSearch.java
@@ -3,6 +3,7 @@ package org.koreait.admin.member.controllers;
 import lombok.Data;
 import org.koreait.global.paging.CommonSearch;
 import org.koreait.member.constants.Authority;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -12,6 +13,10 @@ public class MemberSearch extends CommonSearch {
     private List<String> email;
     private List<Authority> authority;
     private String dateType;
+
+    @DateTimeFormat(pattern="yyyy-MM-dd")
     private LocalDate sDate;
+
+    @DateTimeFormat(pattern="yyyy-MM-dd")
     private LocalDate eDate;
 }

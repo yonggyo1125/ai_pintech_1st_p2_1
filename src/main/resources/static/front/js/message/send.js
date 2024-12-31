8a5b91551ab818cbfe067e5df580fbaa484dac28
yonggyo1125 <yonggyo00@kakao.com>
2024-12-31 16:11:54 +0900

    .

--- src/main/resources/static/front/js/message/send.js
+++ src/main/resources/static/front/js/message/send.js
@@ -1,4 +1,8 @@
 window.addEventListener("DOMContentLoaded", function() {
-    ClassicEditor.create(document.getElementById("content"))
-        .then((editor) => console.log(editor));
+    const { loadEditor } = commonLib;
+
+    loadEditor("content", 450)
+        .then((editor) => {
+            window.editor = editor; // 전역 변수로 등록, then 구간 외부에서도 접근 가능하게 처리
+        });
 });

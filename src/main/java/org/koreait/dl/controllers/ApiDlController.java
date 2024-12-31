7c0b4e59c5efabea7afe172bca8794f092230b70
yonggyo1125 <yonggyo00@kakao.com>
2024-12-31 11:11:26 +0900

    .

--- src/main/java/org/koreait/dl/controllers/ApiDlController.java
+++ src/main/java/org/koreait/dl/controllers/ApiDlController.java
@@ -3,6 +3,7 @@ package org.koreait.dl.controllers;
 import lombok.RequiredArgsConstructor;
 import org.koreait.dl.entities.TrainItem;
 import org.koreait.dl.services.PredictService;
+import org.koreait.dl.services.SentimentService;
 import org.koreait.dl.services.TrainService;
 import org.koreait.global.rests.JSONData;
 import org.springframework.context.annotation.Profile;
@@ -18,6 +19,7 @@ public class ApiDlController {
 
     private final PredictService predictService;
     private final TrainService trainService;
+    private final SentimentService sentimentService;
 
     @GetMapping("/data")
     public List<TrainItem> sendData(@RequestParam(name="mode", required = false) String mode) {
@@ -30,6 +32,13 @@ public class ApiDlController {
     public JSONData predict(@RequestParam("items") List<int[]> items) {
 
         int[] predictions = predictService.predict(items);
+
+        return new JSONData(predictions);
+    }
+
+    @PostMapping("/sentiment")
+    public JSONData sentiment(@RequestParam("items") List<String> items) {
+        double[] predictions = sentimentService.predict(items);
 
         return new JSONData(predictions);
     }
---
+++ src/test/java/org/koreait/dl/services/SentimentServiceTest.java
@@ -0,0 +1,26 @@
+package org.koreait.dl.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Arrays;
+import java.util.List;
+
+@SpringBootTest
+@ActiveProfiles({"default", "dl"})
+public class SentimentServiceTest {
+
+    @Autowired
+    private SentimentService service;
+
+    @Test
+    void predictTest() {
+        List<String> items = List.of("재미없음", "재미있음, 10점 만점");
+
+        double[] predictions = service.predict(items);
+
+        System.out.println(Arrays.toString(predictions));
+    }
+}

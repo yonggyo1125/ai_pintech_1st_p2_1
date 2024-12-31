26725f36c075392bb6e61ae044b5da43f1ef6917
yonggyo1125 <yonggyo00@kakao.com>
2024-12-31 10:50:47 +0900

    .

--- src/main/java/org/koreait/dl/services/SentimentService.java
+++ src/main/java/org/koreait/dl/services/SentimentService.java
@@ -1,9 +1,16 @@
 package org.koreait.dl.services;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
+import java.io.InputStream;
+import java.util.List;
+
+@Lazy
 @Service
 @Profile("dl")
 public class SentimentService {
@@ -13,4 +20,26 @@ public class SentimentService {
 
     @Value("${python.script2.path}")
     private String scriptPath;
+
+    @Value("${python.bert.path}")
+    private String bertPath;
+
+    @Autowired
+    private ObjectMapper om;
+
+    public double[] predict(List<String> items) {
+        try {
+            String data = om.writeValueAsString(items);
+
+            ProcessBuilder builder = new ProcessBuilder(runPath, scriptPath + "naver.py", bertPath, data);
+            Process process = builder.start();
+            InputStream in = process.getInputStream();
+            return om.readValue(in.readAllBytes(), double[].class);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
 }

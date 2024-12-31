c7ca64848fabc643ae6030aa4495c9928ebd4186
yonggyo1125 <yonggyo00@kakao.com>
2024-12-31 14:48:38 +0900

    .

--- src/main/java/org/koreait/message/controllers/MessageController.java
+++ src/main/java/org/koreait/message/controllers/MessageController.java
@@ -4,9 +4,11 @@ import lombok.RequiredArgsConstructor;
 import org.koreait.global.annotations.ApplyErrorPage;
 import org.koreait.global.libs.Utils;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @Controller
 @ApplyErrorPage
@@ -22,7 +24,10 @@ public class MessageController {
      * @return
      */
     @GetMapping
-    public String form() {
+    public String form(@ModelAttribute RequestMessage form, Model model) {
+        commonProcess("send", model);
+
+        form.setGid(UUID.randomUUID().toString());
 
         return utils.tpl("message/form");
     }

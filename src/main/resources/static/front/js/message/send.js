5f56683d8182df1986127a67125dd700e11ab174
yonggyo1125 <yonggyo00@kakao.com>
2024-12-31 17:14:58 +0900

    .

--- src/main/resources/static/front/js/message/send.js
+++ src/main/resources/static/front/js/message/send.js
@@ -18,6 +18,13 @@ function callbackFileUpload(files) {
     }
 
     const imageUrls = [];
+
+    const targetEditor = document.getElementById("editor-files");
+    const targetAttach = document.getElementById("attach-files");
+    const tpl = document.getElementById("tpl-file-item").innerHTML;
+
+    const domParser = new DOMParser();
+
     for (const {seq, fileUrl, fileName, location} of files) {
         if (location === 'editor') { // 에디터에 추가될 이미지
             imageUrls.push(fileUrl);

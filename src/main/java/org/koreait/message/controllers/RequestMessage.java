050e33a89ae714b12bd0edeb7b20f1b492117c7e
yonggyo1125 <yonggyo00@kakao.com>
2024-12-31 15:09:28 +0900

    .

--- src/main/java/org/koreait/message/controllers/RequestMessage.java
+++ src/main/java/org/koreait/message/controllers/RequestMessage.java
@@ -1,5 +1,6 @@
 package org.koreait.message.controllers;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
@@ -12,6 +13,7 @@ public class RequestMessage {
      *    필수가 되는 조건 : 회원이 다른 회원에게 쪽지를 보내는 경우
      *    필수가 아닌 조건 : 관리자가 공지사항(notice)으로 쪽지를 보내는 경우
      */
+    @Email
     private String email;
 
     @NotBlank
---
+++ src/main/java/org/koreait/message/validators/MessageValidator.java
@@ -0,0 +1,38 @@
+package org.koreait.message.validators;
+
+import lombok.RequiredArgsConstructor;
+import org.koreait.member.libs.MemberUtil;
+import org.koreait.message.controllers.RequestMessage;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Lazy
+@Component
+@RequiredArgsConstructor
+public class MessageValidator implements Validator {
+
+    private final MemberUtil memberUtil;
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return clazz.isAssignableFrom(RequestMessage.class);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        RequestMessage form = (RequestMessage) target;
+        String email = form.getEmail();
+        boolean notice = form.isNotice();
+        if (!memberUtil.isAdmin() && notice) { // 관리자가 아니지만 공지 쪽지이면 X
+            notice = false;
+            form.setNotice(notice);
+        }
+
+        if (!memberUtil.isAdmin() && !notice && !StringUtils.hasText(email)) {
+            errors.rejectValue("email", "NotBlank");
+        }
+    }
+}

ce484b9dad5d16388d991db8d23e803d48bd1dd0
yonggyo1125 <yonggyo00@kakao.com>
2025-01-02 10:49:14 +0900

    .

--- src/test/java/org/koreait/dl/controllers/ApiDlControllerTest.java
+++ src/test/java/org/koreait/dl/controllers/ApiDlControllerTest.java
@@ -4,8 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 @SpringBootTest
 @ActiveProfiles({"default", "dl"})
@@ -16,7 +20,12 @@ public class ApiDlControllerTest {
     private MockMvc mockMvc;
 
     @Test
-    void sentimentTest() {
+    void sentimentTest() throws Exception {
 
+        mockMvc.perform(post("/api/dl/sentiment")
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .param("items", "재미없음", "재미있음")
+                )
+                .andDo(print());
     }
 }

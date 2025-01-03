e7706a7043426110065e5ac837c4ddcdfe43570d
yonggyo1125 <yonggyo00@kakao.com>
2025-01-03 10:13:45 +0900

    .

--- src/test/java/org/koreait/member/services/MemberUpdateServiceTest2.java
+++ src/test/java/org/koreait/member/services/MemberUpdateServiceTest2.java
@@ -1,9 +1,14 @@
 package org.koreait.member.services;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.koreait.member.constants.Gender;
+import org.koreait.member.controllers.RequestJoin;
+import org.koreait.member.entities.Member;
+import org.koreait.member.repositories.MemberRepository;
 import org.koreait.mypage.controllers.RequestProfile;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -20,21 +25,39 @@ public class MemberUpdateServiceTest2 {
     @Autowired
     private MemberUpdateService service;
 
+    @Autowired
+    private MemberRepository repository;
+
+    @Autowired
+    private ModelMapper mapper;
+
+    private RequestProfile profile;
+
+    @BeforeEach
+    void init() {
+        RequestJoin form = new RequestJoin();
+        form.setEmail("user01@test.org");
+        form.setName("이이름");
+        form.setNickName("닉네임");
+        form.setZipCode("0000");
+        form.setAddress("주소");
+        form.setAddressSub("나머지 주소");
+        form.setGender(Gender.MALE);
+        form.setBirthDt(LocalDate.now());
+        form.setPassword("_aA123456");
+        form.setConfirmPassword(form.getPassword());
+
+        service.process(form);
+
+        Member member = repository.findByEmail(form.getEmail()).orElse(null);
+        profile = mapper.map(member, RequestProfile.class);
+    }
+
     @Test
     @DisplayName("회원정보 수정 성공시 예외가 발생하지 않는 테스트")
     void updateSuccessTest() {
         assertDoesNotThrow(() -> {
-            RequestProfile form = new RequestProfile();
-            form.setEmail("user01@test.org");
-            form.setName("이이름");
-            form.setNickName("닉네임");
-            form.setZipCode("0000");
-            form.setAddress("주소");
-            form.setAddressSub("나머지 주소");
-            form.setGender(Gender.MALE);
-            form.setBirthDt(LocalDate.now());
-
-            service.process(form);
+            service.process(profile);
         });
     }
 }

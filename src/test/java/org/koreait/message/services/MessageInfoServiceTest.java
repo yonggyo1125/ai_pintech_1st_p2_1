b81eb589093efdd315bda91a27e6e1436337f86c
yonggyo1125 <yonggyo00@kakao.com>
2025-01-03 15:19:46 +0900

    .

--- src/test/java/org/koreait/message/services/MessageInfoServiceTest.java
+++ src/test/java/org/koreait/message/services/MessageInfoServiceTest.java
@@ -1,5 +1,8 @@
 package org.koreait.message.services;
 
+import org.koreait.member.entities.Member;
+import org.koreait.member.repositories.MemberRepository;
+import org.koreait.member.services.MemberUpdateService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -10,4 +13,16 @@ public class MessageInfoServiceTest {
 
     @Autowired
     private MessageInfoService infoService;
+
+    @Autowired
+    private MessageSendService sendService;
+
+    @Autowired
+    private MemberUpdateService updateService;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    private Member sender;
+    private Member receiver;
 }

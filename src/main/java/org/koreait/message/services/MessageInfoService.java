a3c9ac0397998786e1a80b1bcc49b9adca3eac0d
yonggyo1125 <yonggyo00@kakao.com>
2025-01-03 13:20:32 +0900

    .

--- src/main/java/org/koreait/message/services/MessageInfoService.java
+++ src/main/java/org/koreait/message/services/MessageInfoService.java
@@ -1,12 +1,18 @@
 package org.koreait.message.services;
 
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.koreait.global.paging.ListData;
 import org.koreait.message.controllers.MessageSearch;
 import org.koreait.message.entities.Message;
+import org.koreait.message.entities.QMessage;
+import org.koreait.message.exceptions.MessageNotFoundException;
 import org.koreait.message.repositories.MessageRepository;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Lazy
 @Service
@@ -14,6 +20,7 @@ import org.springframework.stereotype.Service;
 public class MessageInfoService {
 
     private final MessageRepository messageRepository;
+    private final JPAQueryFactory queryFactory;
 
     /**
      * 쪽지 하나 조회
@@ -23,9 +30,11 @@ public class MessageInfoService {
      */
     public Message get(Long seq) {
 
-        Message item = messageRepository.findById(seq).orElseThrow();
+        Message item = messageRepository.findById(seq).orElseThrow(MessageNotFoundException::new);
 
-        return null;
+        addInfo(item); // 추가 정보 처리
+
+        return item;
     }
 
     /**
@@ -35,6 +44,28 @@ public class MessageInfoService {
      * @return
      */
     public ListData<Message> getList(MessageSearch search) {
+        int page = Math.max(search.getPage(), 1);
+        int limit = search.getLimit();
+        limit = limit < 1 ? 20 : limit;
+        int offset = (page - 1) * limit;
+
+        BooleanBuilder andBuilder = new BooleanBuilder();
+        QMessage message = QMessage.message;
+
+        List<Message> items = queryFactory.selectFrom(message)
+                .leftJoin(message.receiver)
+                .fetchJoin()
+                .where(andBuilder)
+                .limit(limit)
+                .offset(offset)
+                .orderBy(message.createdAt.desc())
+                .fetch();
+
+        items.forEach(this::addInfo); // 추가 정보 처리
+
+        long total = messageRepository.count(andBuilder);
+
+
 
         return null;
     }

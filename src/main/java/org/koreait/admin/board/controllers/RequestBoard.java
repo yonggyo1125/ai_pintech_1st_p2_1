37e35c1e474d5fa3fc38e251a3b352e969dbacc4
yonggyo1125 <yonggyo00@kakao.com>
2025-01-07 13:12:58 +0900

    .

---
+++ src/main/java/org/koreait/admin/board/controllers/RequestBoard.java
@@ -0,0 +1,31 @@
+package org.koreait.admin.board.controllers;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import org.koreait.member.constants.Authority;
+
+import java.util.List;
+
+@Data
+public class RequestBoard {
+    @NotBlank
+    private String bid; // 게시판 아이디
+
+    @NotBlank
+    private String name; // 게시판 명
+
+    private boolean open;
+    private String category;  // 줄개행 문자로 여러 분류를 등록
+    private int rowsPerPage; // 1페이지당 게시글 갯수 
+    private int pageRanges; // front 뷰일때 노출되는 페이지 링크 갯수
+    private int pageRangesMobile; // mobile 뷰일때 노출되는 페이지 링크 갯수
+    private boolean useEditor; // 에디터 사용 여부
+    private boolean useEditorImage; // 에디터 첨부 이미지 사용 여부
+    private boolean useAttachFile; // 다운로드용 첨부 파일 사용 여부
+
+    private List<Authority> listAuthority; // 목록 접근 권한 
+    private List<Authority> viewAuthority; // 글보기 접근 권한
+    private List<Authority> writeAuthority; // 글쓰기, 수정, 삭제 권한
+    private List<Authority> commentAuthority; // 댓글 작성 권한
+
+}
--- src/main/java/org/koreait/member/constants/Authority.java
+++ src/main/java/org/koreait/member/constants/Authority.java
@@ -5,6 +5,7 @@ package org.koreait.member.constants;
  *
  */
 public enum Authority {
+    ALL, // 모든 사용자(일반회원 + 부관리자 + 최고 관리자 + 비회원)
     USER, // 일반 회원
     MANAGER, // 부관리자
     ADMIN // 최고 관리자

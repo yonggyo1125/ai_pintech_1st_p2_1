4ed66f78e6c90eea2f5fed6c192fbbda844f2c27
yonggyo1125 <yonggyo00@kakao.com>
2025-01-07 12:21:10 +0900

    .

---
+++ src/main/java/org/koreait/admin/board/controllers/BoardController.java
@@ -0,0 +1,97 @@
+package org.koreait.admin.board.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.koreait.admin.global.menu.SubMenus;
+import org.koreait.global.annotations.ApplyErrorPage;
+import org.koreait.global.libs.Utils;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@ApplyErrorPage
+@RequiredArgsConstructor
+@Controller("adminBoardController")
+@RequestMapping("/admin/board")
+public class BoardController implements SubMenus {
+
+    private final Utils utils;
+
+    @Override
+    @ModelAttribute("menuCode")
+    public String menuCode() {
+        return "board";
+    }
+
+    /**
+     * 게시판 목록
+     *
+     * @param model
+     * @return
+     */
+    @GetMapping("/list")
+    public String list(Model model) {
+        commonProcess("list", model);
+
+        return "admin/board/list";
+    }
+
+    /**
+     * 게시판 설정 등록
+     *
+     * @param model
+     * @return
+     */
+    @GetMapping("/add")
+    public String add(Model model) {
+        commonProcess("add", model);
+
+        return "admin/board/add";
+    }
+
+    /**
+     * 게시판 설정 수정
+     * @param bid
+     * @param model
+     * @return
+     */
+    @GetMapping("/edit/{bid}")
+    public String edit(@PathVariable("bid") String bid, Model model) {
+        commonProcess("edit", model);
+
+        return "admin/board/edit";
+    }
+
+    /**
+     * 게시판 등록, 수정 처리
+     *
+     * @return
+     */
+    @PostMapping("/save")
+    public String save() {
+
+        return "redirect:/admin/board/list";
+    }
+
+    /**
+     * 게시글 관리
+     *
+     * @param model
+     * @return
+     */
+    @GetMapping("/posts")
+    public String posts(Model model) {
+        commonProcess("posts", model);
+
+        return "admin/board/posts";
+    }
+
+    /**
+     * 공통 처리 부분
+     *
+     * @param mode
+     * @param model
+     */
+    private void commonProcess(String mode, Model model) {
+
+    }
+}

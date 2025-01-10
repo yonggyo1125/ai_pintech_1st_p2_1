51ed751293e9f786b35ccc66ba8748126410686b
yonggyo1125 <yonggyo00@kakao.com>
2025-01-10 15:08:43 +0900

    .

--- src/main/java/org/koreait/member/social/controllers/SocialController.java
+++ src/main/java/org/koreait/member/social/controllers/SocialController.java
@@ -1,24 +1,46 @@
 package org.koreait.member.social.controllers;
 
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.koreait.global.annotations.ApplyErrorPage;
+import org.koreait.global.exceptions.scripts.AlertBackException;
+import org.koreait.global.libs.Utils;
+import org.koreait.member.social.constants.SocialChannel;
 import org.koreait.member.social.services.KakaoLoginService;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-
-@RestController
+@Controller
+@ApplyErrorPage
 @RequestMapping("/member/social")
 @RequiredArgsConstructor
 public class SocialController {
 
     private final KakaoLoginService kakaoLoginService;
+    private final HttpSession session;
+    private final Utils utils;
 
     @GetMapping("/callback/kakao")
-    public void callback(@RequestParam(name="code", required = false) String code) {
+    public String callback(@RequestParam(name="code", required = false) String code) {
 
         String token = kakaoLoginService.getToken(code);
-        System.out.println(token);
+        if (!StringUtils.hasText(token)) {
+            throw new AlertBackException(utils.getMessage("UnAuthorized"), HttpStatus.UNAUTHORIZED);
+        }
+
+        boolean result = kakaoLoginService.login(token);
+        if (result) { // 로그인 성공
+            return "redirect:/";
+        }
+
+        // 소셜 회원 미가입 -> 회원가입 페이지 이동
+        session.setAttribute("socialChannel", SocialChannel.KAKAO);
+        session.setAttribute("socialToken", token);
+
+        return "redirect:/member/agree";
     }
 }

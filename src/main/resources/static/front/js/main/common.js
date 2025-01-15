dfc9720f3277ca6f4f642b912e60d40fa6d51ea2
yonggyo1125 <yonggyo00@kakao.com>
2025-01-15 17:22:11 +0900

    .

---
+++ src/main/resources/static/front/js/main/common.js
@@ -0,0 +1,8 @@
+window.addEventListener("DOMContentLoaded", function() {
+    var swiper = new Swiper(".mySwiper", {
+          navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+          },
+        });
+});

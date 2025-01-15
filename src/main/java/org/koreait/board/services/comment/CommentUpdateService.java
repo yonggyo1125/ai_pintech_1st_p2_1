286ad04f14ed0ad40a37e9796f6fac37f1dab32f
yonggyo1125 <yonggyo00@kakao.com>
2025-01-15 12:04:55 +0900

    .

--- src/main/java/org/koreait/board/services/comment/CommentUpdateService.java
+++ src/main/java/org/koreait/board/services/comment/CommentUpdateService.java
@@ -5,7 +5,9 @@ import lombok.RequiredArgsConstructor;
 import org.koreait.board.controllers.RequestComment;
 import org.koreait.board.entities.BoardData;
 import org.koreait.board.entities.CommentData;
+import org.koreait.board.entities.QCommentData;
 import org.koreait.board.exceptions.CommentNotFoundException;
+import org.koreait.board.repositories.BoardDataRepository;
 import org.koreait.board.repositories.CommentDataRepository;
 import org.koreait.board.services.BoardInfoService;
 import org.koreait.member.libs.MemberUtil;
@@ -22,6 +24,7 @@ import java.util.Objects;
 public class CommentUpdateService {
 
     private final CommentDataRepository commentDataRepository;
+    private final BoardDataRepository boardDataRepository;
     private final BoardInfoService boardInfoService;
     private final MemberUtil memberUtil;
     private final HttpServletRequest request;
@@ -61,6 +64,24 @@ public class CommentUpdateService {
 
         commentDataRepository.saveAndFlush(item);
 
+        // 댓글 갯수 업데이트
+        updateCount(boardDataSeq);
+
         return item;
+    }
+
+    /**
+     * 게시글 번호로 총 댓글 갯수 반영
+     *
+     * @param seq : 게시글 번호
+     */
+    public void updateCount(Long seq) {
+        QCommentData commentData = QCommentData.commentData;
+        long total = commentDataRepository.count(commentData.data.seq.eq(seq)); // 게시글별 댓글 갯수
+        BoardData item = boardDataRepository.findById(seq).orElse(null);
+        if (item != null) {
+            item.setCommentCount(total);
+            boardDataRepository.saveAndFlush(item);
+        }
     }
 }

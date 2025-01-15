c7b0aaa42aecf4c32216150d0cac2c36ef83c2b9
yonggyo1125 <yonggyo00@kakao.com>
2025-01-15 17:45:22 +0900

    .

--- src/main/resources/static/front/js/main/common.js
+++ src/main/resources/static/front/js/main/common.js
@@ -1,8 +1,10 @@
 window.addEventListener("DOMContentLoaded", function() {
-    var swiper = new Swiper(".mySwiper", {
+    /* 메인 배너 S */
+    new Swiper(".main-banner .banners", {
           navigation: {
             nextEl: ".swiper-button-next",
             prevEl: ".swiper-button-prev",
           },
-        });
+    });
+    /* 메인 배너 E */
 });
